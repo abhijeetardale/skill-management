ad36b1cd492928c5fc954c35151a1df477a8d1c6
abhijeetardale <abhijeet.ardale@yahoo.co.in>
2020-03-15 22:28:40 +0000

    test passed

--- src/main/java/com/nhs/model/Organisation.java
+++ src/main/java/com/nhs/model/Organisation.java
@@ -1,4 +1,35 @@
 package com.nhs.model;
 
-public class Organisation {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "ORGANISATION")
+public class Organisation implements Serializable {
+
+    @Column(name = "orgId")
+    @Id
+    Integer orgId;
+
+    @Column(name = "orgName")
+    String orgName;
+
+    public Integer getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(Integer orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
 }
